f75170f40490e6afe82edd4c565932a407be74f1
markkirivera <markbalazo14@gmail.com>
2019-01-31 10:16:38 +0800

    Minima upload
    upload minima static web page

---
+++ script/script.js
@@ -0,0 +1,9 @@
+// $(document).ready(function{
+//     $('#sec1').bind('inview', function(_event, visible){
+//         if(visible == true){
+//             $('#sec1').addClass('animated fadeInLeft delay-2s');
+//         }else{
+//             $('#sec1').removeClass('animated fadeInLeft delay-2s');
+//         }
+//     })
+// })
